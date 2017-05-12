20ff244658a1e7b3e1350d99b8b3e266fabaefce
Ian Hattendorf <ianhattendorf@gmail.com>
2017-05-12 15:48:44 -0700

    JPA data type changes
    Upgrade to hibernate 5.2.x to correctly serialize ZonedDateTime

--- src/main/java/com/ianhattendorf/sensi/sensimonitor/domain/Status.java
+++ src/main/java/com/ianhattendorf/sensi/sensimonitor/domain/Status.java
@@ -2,76 +2,76 @@ package com.ianhattendorf.sensi.sensimonitor.domain;
 
 import com.ianhattendorf.sensiapi.response.data.OperationalStatus;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.ZonedDateTime;
 
 @Entity
 public final class Status {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private ZonedDateTime timestamp;
-    private Integer temperature;
-    private Integer humidity;
-    private Integer batteryVoltage;
+    private Integer id;
+    @Column(nullable = false)
+    private ZonedDateTime updatedAt;
+    private Short temperature;
+    private Short humidity;
+    private Short batteryVoltage;
+    @Column(length = 4)
     private String running;
     private Boolean lowPower;
+    @Column(length = 4)
     private String operatingMode;
-    private Integer powerStatus;
+    private Short powerStatus;
 
     protected Status() {}
 
     public Status(OperationalStatus operationalStatus) {
-        timestamp = ZonedDateTime.now();
-        temperature = operationalStatus.getTemperature() == null ? null : operationalStatus.getTemperature().getF();
-        humidity = operationalStatus.getHumidity();
-        batteryVoltage = operationalStatus.getBatteryVoltage();
+        updatedAt = ZonedDateTime.now();
+        temperature = operationalStatus.getTemperature() == null ? null : operationalStatus.getTemperature().getF().shortValue();
+        humidity = operationalStatus.getHumidity().shortValue();
+        batteryVoltage = operationalStatus.getBatteryVoltage().shortValue();
         running = operationalStatus.getRunning() == null ? null : operationalStatus.getRunning().getMode();
         lowPower = operationalStatus.getLowPower();
         operatingMode = operationalStatus.getOperatingMode();
-        powerStatus = operationalStatus.getPowerStatus();
+        powerStatus = operationalStatus.getPowerStatus().shortValue();
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public ZonedDateTime getTimestamp() {
-        return timestamp;
+    public ZonedDateTime getUpdatedAt() {
+        return updatedAt;
     }
 
-    public void setTimestamp(ZonedDateTime timestamp) {
-        this.timestamp = timestamp;
+    public void setUpdatedAt(ZonedDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
-    public Integer getTemperature() {
+    public Short getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(Integer temperature) {
+    public void setTemperature(Short temperature) {
         this.temperature = temperature;
     }
 
-    public Integer getHumidity() {
+    public Short getHumidity() {
         return humidity;
     }
 
-    public void setHumidity(Integer humidity) {
+    public void setHumidity(Short humidity) {
         this.humidity = humidity;
     }
 
-    public Integer getBatteryVoltage() {
+    public Short getBatteryVoltage() {
         return batteryVoltage;
     }
 
-    public void setBatteryVoltage(Integer batteryVoltage) {
+    public void setBatteryVoltage(Short batteryVoltage) {
         this.batteryVoltage = batteryVoltage;
     }
 
@@ -99,11 +99,11 @@ public final class Status {
         this.operatingMode = operatingMode;
     }
 
-    public Integer getPowerStatus() {
+    public Short getPowerStatus() {
         return powerStatus;
     }
 
-    public void setPowerStatus(Integer powerStatus) {
+    public void setPowerStatus(Short powerStatus) {
         this.powerStatus = powerStatus;
     }
 
@@ -111,7 +111,7 @@ public final class Status {
     public String toString() {
         final StringBuilder sb = new StringBuilder("Status{");
         sb.append("id=").append(id);
-        sb.append(", timestamp=").append(timestamp);
+        sb.append(", updatedAt=").append(updatedAt);
         sb.append(", temperature=").append(temperature);
         sb.append(", humidity=").append(humidity);
         sb.append(", batteryVoltage=").append(batteryVoltage);
--- src/main/java/com/ianhattendorf/sensi/sensimonitor/domain/StatusRepository.java
+++ src/main/java/com/ianhattendorf/sensi/sensimonitor/domain/StatusRepository.java
@@ -2,5 +2,5 @@ package com.ianhattendorf.sensi.sensimonitor.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface StatusRepository extends CrudRepository<Status, Long> {
+public interface StatusRepository extends CrudRepository<Status, Integer> {
 }
