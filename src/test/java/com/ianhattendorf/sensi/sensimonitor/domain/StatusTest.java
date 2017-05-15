2802778597b8ad5da52c7c130f2b776f4f70cb41
Ian Hattendorf <ianhattendorf@gmail.com>
2017-05-14 23:28:10 -0700

    Update setPoint test
    Check additional logic from EnvironmentalControls

--- src/test/java/com/ianhattendorf/sensi/sensimonitor/domain/StatusTest.java
+++ src/test/java/com/ianhattendorf/sensi/sensimonitor/domain/StatusTest.java
@@ -1,9 +1,6 @@
 package com.ianhattendorf.sensi.sensimonitor.domain;
 
-import com.ianhattendorf.sensi.sensiapi.response.data.OperationalStatus;
-import com.ianhattendorf.sensi.sensiapi.response.data.ScheduleTemps;
-import com.ianhattendorf.sensi.sensiapi.response.data.Temperature;
-import com.ianhattendorf.sensi.sensiapi.response.data.Update;
+import com.ianhattendorf.sensi.sensiapi.response.data.*;
 import com.ianhattendorf.sensi.sensimonitor.util.Mapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,14 +16,17 @@ public final class StatusTest {
     public void setUp() {
         update = new Update();
         update.setOperationalStatus(new OperationalStatus());
+        update.getOperationalStatus().setScheduleTemps(
+                new ScheduleTemps(null, null,
+                        new Temperature(62, 17), new Temperature(79, 26)));
+        update.setEnvironmentControls(new EnvironmentControls());
+        update.getEnvironmentControls().setCoolSetpoint(new Temperature(75, 23));
+        update.getEnvironmentControls().setHeatSetpoint(new Temperature(70, 21));
+        update.getEnvironmentControls().setScheduleMode("On");
     }
 
     @Test
     public void testSetPoint() {
-        update.getOperationalStatus().setScheduleTemps(
-                new ScheduleTemps(null, null,
-                        new Temperature(62, 17), new Temperature(79, 26)));
-
         Status status = Mapper.updateToStatus(update);
         assertNull(status.getSetPoint());
 
@@ -43,6 +43,28 @@ public final class StatusTest {
         assertNull(status.getSetPoint());
 
         update.getOperationalStatus().setOperatingMode("asdf");
+        status = Mapper.updateToStatus(update);
+        assertNull(status.getSetPoint());
+
+        update.getEnvironmentControls().setScheduleMode("Off");
+        update.getOperationalStatus().setOperatingMode("Cool");
+        status = Mapper.updateToStatus(update);
+        assertEquals(75, status.getSetPoint().shortValue());
+
+        update.getOperationalStatus().setOperatingMode("Heat");
+        status = Mapper.updateToStatus(update);
+        assertEquals(70, status.getSetPoint().shortValue());
+
+        EnvironmentControls environmentControls = update.getEnvironmentControls();
+        update.setEnvironmentControls(null);
+        status = Mapper.updateToStatus(update);
+        assertNull(status.getSetPoint());
+
+        update.setOperationalStatus(null);
+        status = Mapper.updateToStatus(update);
+        assertNull(status.getSetPoint());
+
+        update.setEnvironmentControls(environmentControls);
         status = Mapper.updateToStatus(update);
         assertNull(status.getSetPoint());
     }
