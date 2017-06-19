2399b3b5b9aa7e7a635cf62b66534eb12a86d657
Ian Hattendorf <ianhattendorf@gmail.com>
2017-06-19 11:49:38 -0700

    Build OS packages (rpm/deb)

--- rpm/preinstall.sh
+++ rpm/preinstall.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-/usr/bin/getent group %{name} > /dev/null || /usr/sbin/groupadd -r %{name}
-/usr/bin/getent passwd %{name} > /dev/null || /usr/sbin/useradd -r -d /opt/%{name} -s /sbin/nologin -g %{name} %{name}
+/usr/bin/getent group sensi-monitor > /dev/null || /usr/sbin/groupadd -r sensi-monitor
+/usr/bin/getent passwd sensi-monitor > /dev/null || /usr/sbin/useradd -r -d /opt/sensi-monitor -s /sbin/nologin -g sensi-monitor sensi-monitor
